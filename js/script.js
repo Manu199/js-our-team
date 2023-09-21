135e16740a15c57d79ad358fcaf3d6cb6e017c2d
Manu199 <manu199gh@gmail.com>
2023-09-21 17:53:53 +0200

    [add] list and images on html

--- js/script.js
+++ js/script.js
@@ -5,38 +5,47 @@ const team = [
   {
     nome : 'Wayne Barnett',
     title : 'Founder & CEO',
-    image : '<img> src="img/wayne-barnett-founder-ceo.jpg"</img>' ,
+    image : 'img/wayne-barnett-founder-ceo.jpg' ,
   },
   {
     nome : 'Angela Carol',
     title : 'Chief Editor',
-    image : 'angela-caroll-chief-editor.jpg',
+    image : 'img/angela-caroll-chief-editor.jpg',
   },
   {
     nome : 'Walter Gordon',
     title : 'Office Manager',
-    image : 'walter-gordon-office-manager.jpg',
+    image : 'img/walter-gordon-office-manager.jpg',
   },
   {
     nome : 'Angela Lope',
     title : 'Social Media Manager',
-    image : 'angela-lopez-social-media-manager.jpg',
+    image : 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     nome : 'Scott Estrada',
     title : 'Developer',
-    image : 'scott-estrada-developer.jpg',
+    image : 'img/scott-estrada-developer.jpg',
   },
   {
     nome : 'Barbara Ramos',
     title : 'Graphic Designer',
-    image : 'barbara-ramos-graphic-designer.jpg',
+    image : 'img/barbara-ramos-graphic-designer.jpg',
   }
 ];
 
-console.log(team);
+const ul = document.querySelector('ul');
 
-document.getElementById('output').innerHTML = team;
+// for(let i = 0; i < team.length; i++){
+//   const card = team[i];
+ 
+// }
+
+for (let card of team) {
+console.log(card);
+ul.insertAdjacentHTML('beforeend', `<li>Nome: ${card.nome} - Titolo: ${card.title} <br><img src="${card.image}" alt="${card.nome}"></li>`);
+}
+// document.getElementById('output').innerHTML = team;
 // - creare un ciclo per avere i valari dei nomi, ruoli e foto.
 // - stampare in console
 // - stampare nel DOM.
